a15f7ee6c4a993020125ea07e984028eafb9791f
Anna König <mail@annakoenig.de>
2015-08-30 21:40:43 +0200

    news-page angefangen

--- wp-content/themes/child_14_hoesli_nicole/page-news.php
+++ wp-content/themes/child_14_hoesli_nicole/page-news.php
@@ -15,16 +15,29 @@
 get_header(); ?>
 
 <div id="main-content" class="main-content">
+	<div class="row">
+		<div class="col-md-4">
+			  <div class="news_title">
+			  	<?php
+				/* translators: %s: Name of current post */
+				the_content();
 
-<?php
-	if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
-		// Include the featured content template.
-		get_template_part( 'featured-content' );
-	}
-?>
+			?> 
+			  </div>
+			  <div class="news-text">
+			  </div>
+		</div>
+
+		<div class="col-md-8">
+		<?php
+			if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
+				// Include the featured content template.
+				get_template_part( 'featured-content' );
+			}
+		?>
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
-				<h1>neues von nicole</h1>
+				
 			<?php
 				// Start the Loop.
 				while ( have_posts() ) : the_post();
@@ -32,18 +45,14 @@ get_header(); ?>
 					// Include the page content template.
 					get_template_part( 'content', 'page' );
 
-					// If comments are open or we have at least one comment, load up the comment template.
-					if ( comments_open() || get_comments_number() ) {
-						comments_template();
-					}
 				endwhile;
 			?>
-
+		</div>
+		
 		</div><!-- #content -->
 	</div><!-- #primary -->
-	<?php get_sidebar( 'content' ); ?>
+	
 </div><!-- #main-content -->
 
 <?php
-get_sidebar();
 get_footer();
